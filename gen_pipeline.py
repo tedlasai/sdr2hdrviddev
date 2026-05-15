c00069a69f208dde2b5227393bc5ee7bd564c8e5
tedlasai <tedlasai@gmail.com>
2026-05-15 20:54:08 +0000

    siggraph submission done w/ this code

--- gen_pipeline.py
+++ gen_pipeline.py
@@ -186,10 +186,7 @@ def main():
     parser.add_argument(
         "--negative_prompt",
         default=(
-            "static, blurry details, subtitles, style, artwork, image, still, overall gray, "
-            "worst quality, low quality, JPEG compression artifacts, ugly, deformed, extra fingers, "
-            "poorly drawn hands, poorly drawn face, malformed limbs, fused fingers, still frame, "
-            "messy background, three legs, crowded background people, walking backwards"
+            "色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走"
         ),
         help="Negative prompt.",
     )
